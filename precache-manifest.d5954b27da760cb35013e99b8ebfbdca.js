8d879721156e8b092fb7814da86d335f8539a9ca
fabio <fabgmad@gmail.com>
2019-12-03 21:16:47 -0300

    Updates

---
+++ precache-manifest.d5954b27da760cb35013e99b8ebfbdca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f72e924b96d95952e10288e7659a4d5",
+    "url": "/udemy-course-react-developer-monsters-exercise/index.html"
+  },
+  {
+    "revision": "40fa4e05ce46ed6423e7",
+    "url": "/udemy-course-react-developer-monsters-exercise/static/css/main.7f7809a7.chunk.css"
+  },
+  {
+    "revision": "267686363d896558f68d",
+    "url": "/udemy-course-react-developer-monsters-exercise/static/js/2.2d2a8028.chunk.js"
+  },
+  {
+    "revision": "40fa4e05ce46ed6423e7",
+    "url": "/udemy-course-react-developer-monsters-exercise/static/js/main.28dc483c.chunk.js"
+  },
+  {
+    "revision": "b151fb2292663349a588",
+    "url": "/udemy-course-react-developer-monsters-exercise/static/js/runtime-main.eafeb767.js"
+  }
+]);
